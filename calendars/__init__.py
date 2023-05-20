ed4bd63bd4ca8a76d9eb16f0ea2203498c3223a3
Valeriya1904 <maxtrus08@mail.ru>
2023-05-20 18:33:41 +0300

    Initial commit

---
+++ calendars/__init__.py
@@ -0,0 +1,1 @@
+from Bot.ReminderBot.calendars.simple_calendar import calendar_callback as simple_calendar_callback, SimpleCalendar
---
+++ handlers/__init__.py
@@ -0,0 +1,3 @@
+from Bot.ReminderBot.handlers.reminder import remind_register_handlers as reminder
+from Bot.ReminderBot.handlers.other import other_register_handlers as other
+from Bot.ReminderBot.handlers.bug_report import bug_report_register_handlers as bug_report
